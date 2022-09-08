8dd7af8281491e7c8592f6668703e3009c2dff3d
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-09-08 16:30:23 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- print the sting in the put function
+ * return: always zero(0) success
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle \n");
+	return(0);
+}

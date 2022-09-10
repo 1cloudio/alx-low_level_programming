516c8968db464e6d20bb06024e5b2b45bc1b97d2
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-09-10 03:53:08 -0700

    print tebalpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stlib.h>
+/**
+* main - print if number is positive, negative or zero
+* Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'z' ; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}

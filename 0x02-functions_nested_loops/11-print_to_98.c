c92f973949a5296d5a2e981fce84efbaf84d7376
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-09-13 21:24:03 -0700

    zero to hero

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print natural numbers to 98,
+ * followed by a space and separated by coma
+ * @n: begin counting from a
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d,", n--);
+		printf("%dn", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d,", n++);
+		printf("%dn", n);
+
+	}
+}

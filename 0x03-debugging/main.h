094c4fed99013472312887171ad89c166f85b89d
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-09-14 14:17:00 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,11 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 
 void positive_or_negative(i);
 int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_days(int, int);
-#endif
+
+#endif /* MAIN_H */

d7657bbad4af660c11b14699bbdefc790c269f6f
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-09-13 18:46:35 -0700

    print digits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - is a gotten from the modulu of the variable
+ *  @n: is an integer value
+ *  Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	int c = n % 10;
+
+	if (n < 0)
+		n * -1;
+
+	_putchar(n = '0');
+	return (n);
+
+}

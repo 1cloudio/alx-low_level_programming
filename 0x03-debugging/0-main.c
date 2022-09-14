a43e380ef7508fab5e56e170aeec3e5c9db6d30b
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-09-14 09:27:24 -0700

    testing integers

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints negative or positive integer
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+
+}
+
+

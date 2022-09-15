caec46649360339bcb4be877d938e3a86bc78164
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-09-15 12:42:21 -0700

    digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - a function that checks for a digit
+ * @c: charcter to be tasted
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 1) && (c <= 9))
+	{
+		return (1);
+	}
+
+	return (0);
+
+}

593778539c7dc88822f00d9647711956ec09d948
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-09-15 14:30:51 -0700

    is digit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,10 +7,12 @@
  * int prototypes (int)
  */
 
+#include <stdio.h>
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int _putchar(char);
+int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);

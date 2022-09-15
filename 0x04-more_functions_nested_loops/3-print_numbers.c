b1146c5c463a929d34006e15f99b07394ebcca73
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-09-15 13:34:00 -0700

    prints number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int a:
+
+	for (a = 8; a < 18; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+
+}

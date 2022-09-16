6f87b202d3715aa6cb63a826be8d161bfae02c3a
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-09-15 19:34:44 -0700

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,16 +9,18 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int x;
+	x = n;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		num = -num;
+		_putchar(45);
+		x = -n;
 	}
-	if (num > 9)
+	if  (x / 10)
 	{
-		print_number(num / 10);
+		print_number(x / 10);
 	}
-	_putchar(num % 10 + '0');
+	_putchar((x % 10) + '0');
+
 }

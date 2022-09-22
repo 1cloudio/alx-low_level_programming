c0479bb4ed1f9d48278bd1039debddd88385149d
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-09-22 03:32:14 -0700

    magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Add one line to this code, so that the program prints a[2] = 98,
+ * followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int m;
+	int z[5];
+	int *p;
+
+	z[2] = 1024;
+	p = &m;
+
+	*(p + 5) = 98;
+	printf("z[2] =%d\n", z[2]);
+
+	return (0);
+
+}

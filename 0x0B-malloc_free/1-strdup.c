8dd9696ad9f3487c8d82d3e56b59537fbbee2fd1
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-10-04 04:50:56 -0700

    concatination

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,6 +16,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
+		;
 	p = (char *)malloc(sizeof(char) * (i + 1));
 	if (p == NULL)
 		return (NULL);

45c00b0d02ae7f2e4c7278d19192905545d6d46f
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-10-24 21:43:37 +0100

    more singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,39 +1,37 @@
 #include "lists.h"
+
 /**
-  * delete_nodeint_at_index - deletes the node at index.
-  * @head: pointer to pointer of first elemnt of list.
-  * @index: index of node to be deleted.
-  *
-  * Return: 1 if successful, -1 if fails.
-  */
+ * delete_nodeint_at_index - Deletes the node at a given index of a listint_t list.
+ * @head: A pointer to the address of the head of the listint_t list.
+ * @index: The index of the node to be deleted - indices start at 0.
+ *
+ * Return: On success - 1, On failure - -1.
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int count;
-	listint_t *next_node, *temp;
+	listint_t *tmp, *copy = *head;
+	unsigned int node;
 
-	temp = *head;
-	count = 0;
-
-	if (*head == NULL)
+	if (copy == NULL)
 		return (-1);
+
 	if (index == 0)
 	{
-		*head = temp->next;
-		free(temp);
+		*head = (*head)->next;
+		free(copy);
 		return (1);
 	}
 
-	while (temp != NULL && count < index - 1)
+	for (node = 0; node < (index - 1); node++)
 	{
-		temp = temp->next;
-		count++;
-		if (temp == NULL || temp->next == NULL)
+		if (copy->next == NULL)
 			return (-1);
+
+		copy = copy->next;
 	}
 
-	next_node = temp->next->next;
-	free(temp->next);
-	temp->next = next_node;
-
+	tmp = copy->next;
+	copy->next = tmp->next;
+	free(tmp);
 	return (1);
 }
